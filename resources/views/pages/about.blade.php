c5794e4145611cd6219c3be872c35ee58d79a02c
emmanuela <eoa22@njit.edu>
2019-04-13 03:24:58 -0400

    Pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('content')
+    <br>
+    <br>
+    <br>
+    <h2>Our mission</h2>
+
+    <p>To bring the up-to-date news about your favorite sports, players and fantasy games.
+        Our main focus is to inspire every fan and bring excitement to every fan in the world.</p>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.default')
+@section('content')
+    <br>
+    <br>
+
+
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+        <h1>Sport cast!</h1>
+        <p class="lead">We work 24/7 to keep you up-to-date with the latest sport news (soccer, football, nhl, mlb etc.)
+            In other to access the page, please Sign-in or Sign-up.
+        </p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+    </div>
+
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>News</h2>
+            <b>Luke Walton couldn't outcoach the Lakers' dysfunction</b>
+            <p>Walton seemed to be on the clock as soon as the Lakers signed LeBron James, and a season of unrealistic expectations and injuries reached fevered levels in the days before his firing. </p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Videos</h2>
+            <p>As week 2 come to an End, here are the top play of the weekend. </p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Fantasy</h2>
+            <b>NFL Week 3 Power Rankings: Rams are new No. 1... Chaos below</b>
+            <p> There's turnover at the top of the Power Rankings, with new teams cracking the top three. Plus: Why every team can be optimistic</p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+    </div>
+
+
+@stop
